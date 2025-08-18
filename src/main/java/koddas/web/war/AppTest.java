3308a91a7909fb6ee9f989d5b5abed3f6d69f423
vikashbond <vikash.mishra22110@gmail.com>
2025-08-18 10:23:04 +0530

    Create AppTest.java
    new java file added

---
+++ src/main/java/koddas/web/war/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
